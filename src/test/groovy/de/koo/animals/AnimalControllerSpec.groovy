53dfe254c07a802e830467a90ce520c32253d316
Gero Klinkmann <gklinkmann@k-oo.de>
2020-04-24 09:42:03 +0200

    fix AnimalController test

--- src/test/groovy/de/koo/animals/AnimalControllerSpec.groovy
+++ src/test/groovy/de/koo/animals/AnimalControllerSpec.groovy
@@ -1,5 +1,8 @@
 package de.koo.animals
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+
+import grails.plugin.springsecurity.SpringSecurityService
 import grails.testing.gorm.DomainUnitTest
 import grails.testing.web.controllers.ControllerUnitTest
 import grails.validation.ValidationException
@@ -10,9 +13,11 @@ class AnimalControllerSpec extends Specification implements ControllerUnitTest<A
     def populateValidParams(params) {
         assert params != null
 
-        // TODO: Populate valid properties like...
-        //params["name"] = 'someValidName'
-        assert false, "TODO: Provide a populateValidParams() implementation for this generated test suite"
+		params.name='A1'
+		params.cageNr=1
+		params.arrived=new Date()
+		params.approbation='E1'
+		params.gender='m'
     }
 
     void "Test the index action returns the correct model"() {
@@ -51,42 +56,50 @@ class AnimalControllerSpec extends Specification implements ControllerUnitTest<A
 
     void "Test the save action correctly persists"() {
         given:
-        controller.animalService = Mock(AnimalService) {
-            1 * save(_ as Animal)
-        }
+	        controller.animalService = Mock(AnimalService) {
+	            1 * save(_ as Animal)
+	        }
+			controller.springSecurityService = Stub(SpringSecurityService) {
+				isLoggedIn() >> true
+				getPrincipal() >> new org.springframework.security.core.userdetails.User("admin","admin",[new SimpleGrantedAuthority("ROLE_ADMIN")])
+			}
 
         when:"The save action is executed with a valid instance"
-        response.reset()
-        request.contentType = FORM_CONTENT_TYPE
-        request.method = 'POST'
-        populateValidParams(params)
-        def animal = new Animal(params)
-        animal.id = 1
-
-        controller.save(animal)
+	        response.reset()
+	        request.contentType = FORM_CONTENT_TYPE
+	        request.method = 'POST'
+	        populateValidParams(params)
+	        def animal = new Animal(params)
+	        animal.id = 1
+	
+	        controller.save(animal)
 
         then:"A redirect is issued to the show action"
-        response.redirectedUrl == '/animal/show/1'
-        controller.flash.message != null
+	        response.redirectedUrl == '/animal/show/1'
+	        controller.flash.message != null
     }
 
     void "Test the save action with an invalid instance"() {
         given:
-        controller.animalService = Mock(AnimalService) {
-            1 * save(_ as Animal) >> { Animal animal ->
-                throw new ValidationException("Invalid instance", animal.errors)
-            }
-        }
+	        controller.animalService = Mock(AnimalService) {
+	            1 * save(_ as Animal) >> { Animal animal ->
+	                throw new ValidationException("Invalid instance", animal.errors)
+	            }
+	        }
+			controller.springSecurityService = Stub(SpringSecurityService) {
+				isLoggedIn() >> true
+				getPrincipal() >> new org.springframework.security.core.userdetails.User("admin","admin",[new SimpleGrantedAuthority("ROLE_ADMIN")])
+			}
 
         when:"The save action is executed with an invalid instance"
-        request.contentType = FORM_CONTENT_TYPE
-        request.method = 'POST'
-        def animal = new Animal()
-        controller.save(animal)
+	        request.contentType = FORM_CONTENT_TYPE
+	        request.method = 'POST'
+	        def animal = new Animal()
+	        controller.save(animal)
 
         then:"The create view is rendered again with the correct model"
-        model.animal != null
-        view == 'create'
+	        model.animal != null
+	        view == 'create'
     }
 
     void "Test the show action with a null id"() {
@@ -155,41 +168,49 @@ class AnimalControllerSpec extends Specification implements ControllerUnitTest<A
 
     void "Test the update action correctly persists"() {
         given:
-        controller.animalService = Mock(AnimalService) {
-            1 * save(_ as Animal)
-        }
+	        controller.animalService = Mock(AnimalService) {
+	            1 * save(_ as Animal)
+	        }
+			controller.springSecurityService = Stub(SpringSecurityService) {
+				isLoggedIn() >> true
+				getPrincipal() >> new org.springframework.security.core.userdetails.User("admin","admin",[new SimpleGrantedAuthority("ROLE_ADMIN")])
+			}
 
         when:"The save action is executed with a valid instance"
-        response.reset()
-        request.contentType = FORM_CONTENT_TYPE
-        request.method = 'PUT'
-        populateValidParams(params)
-        def animal = new Animal(params)
-        animal.id = 1
-
-        controller.update(animal)
+	        response.reset()
+	        request.contentType = FORM_CONTENT_TYPE
+	        request.method = 'PUT'
+	        populateValidParams(params)
+	        def animal = new Animal(params)
+	        animal.id = 1
+	
+	        controller.update(animal)
 
         then:"A redirect is issued to the show action"
-        response.redirectedUrl == '/animal/show/1'
-        controller.flash.message != null
+	        response.redirectedUrl == '/animal/show/1'
+	        controller.flash.message != null
     }
 
     void "Test the update action with an invalid instance"() {
         given:
-        controller.animalService = Mock(AnimalService) {
-            1 * save(_ as Animal) >> { Animal animal ->
-                throw new ValidationException("Invalid instance", animal.errors)
-            }
-        }
+	        controller.animalService = Mock(AnimalService) {
+	            1 * save(_ as Animal) >> { Animal animal ->
+	                throw new ValidationException("Invalid instance", animal.errors)
+	            }
+	        }
+			controller.springSecurityService = Stub(SpringSecurityService) {
+				isLoggedIn() >> true
+				getPrincipal() >> new org.springframework.security.core.userdetails.User("admin","admin",[new SimpleGrantedAuthority("ROLE_ADMIN")])
+			}
 
         when:"The save action is executed with an invalid instance"
-        request.contentType = FORM_CONTENT_TYPE
-        request.method = 'PUT'
-        controller.update(new Animal())
+	        request.contentType = FORM_CONTENT_TYPE
+	        request.method = 'PUT'
+	        controller.update(new Animal())
 
         then:"The edit view is rendered again with the correct model"
-        model.animal != null
-        view == 'edit'
+	        model.animal != null
+	        view == 'edit'
     }
 
     void "Test the delete action with a null instance"() {
